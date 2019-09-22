a93e3791674f214c9bee742ed5e72f7fc05ab5d6
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2019-09-22 11:54:53 +0300

    The first commit!

--- SDP-Git_Assigment_2/Program.cs
+++ SDP-Git_Assigment_2/Program.cs
@@ -7,6 +7,7 @@ namespace SDP_Git_Assigment_2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("The very first change!");
         }
     }
 }
